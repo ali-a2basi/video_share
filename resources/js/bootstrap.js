0f37cc375b5e31b448c0f84afeccc2854c4bdeeb
Ali Abbasi <ali.abasi@protonmail.com>
2023-12-21 13:01:12 +0330

    webpack configured

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,8 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+import $ from 'jquery';
+window.jQuery = window.$ = $;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
